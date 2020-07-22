e17b9a66d89db14179186fb3404a83498f05efb2
mslooten <marco.slooten@topicus.nl>
2020-07-22 14:50:21 +0200

    Adds netlify toml and purge tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: ["./components/*.js", "./pages/*.js"],
   theme: {
-    extend: {},
+    extend: {}
   },
   variants: {},
-  plugins: [],
-}
+  plugins: []
+};
