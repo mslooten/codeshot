b713a74169e10c31f541bdc4feeb37b5fdddd3c1
mslooten <marco.slooten@topicus.nl>
2020-07-22 15:13:06 +0200

    Try to prevent concatenation for tailwind purge

--- components/ColorBlock.js
+++ components/ColorBlock.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const ColorBlock = (props) => {
   const { color, selected, handleClick } = props;
+  const bgColor = `bg-${color}`;
   return (
     <div
-      className={`w-3 h-3 m-1 rounded-sm bg-${color} hover:opacity-75 cursor-pointer ${
+      className={`w-3 h-3 m-1 rounded-sm ${bgColor} hover:opacity-75 cursor-pointer ${
         selected && "transform scale-150 border-2 border-black border-opacity-25 shadow rounded-full"
       }`}
       key={color}
