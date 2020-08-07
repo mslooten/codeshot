3d58030c9cb5f642e298d1fb5aa1422dec7de7f8
mslooten <marco.slooten@topicus.nl>
2020-08-07 15:25:15 +0200

    Make dir if it doesnt exist

--- snippetshot-web/getFonts.js
+++ snippetshot-web/getFonts.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const https = require("https");
 const fs = require("fs");
 
+!fs.existsSync(`./fonts/`) && fs.mkdirSync(`./fonts/`);
+
 const file = fs.createWriteStream("./fonts/monolisa.css");
 
 const request = https.get(process.env.MONOLISA, function (response) {
