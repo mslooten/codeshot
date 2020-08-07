2b555ec85241c60cd32e9d0e8232faa0c78746b3
mslooten <marco.slooten@topicus.nl>
2020-08-07 15:22:17 +0200

    Adds MonoLisa

--- snippetshot-web/components/Settings.js
+++ snippetshot-web/components/Settings.js
@@ -32,7 +32,7 @@ const Settings = (props) => {
           />
         </label>
         <label className="text-gray-600 text-sm font-bold block">
-          Typeface:
+          Font:
           <div className="block relative w-full mb-4">
             <div className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline font-mono text-gray-700">
               MonoLisa{" "}
